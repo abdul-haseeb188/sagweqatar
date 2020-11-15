2414f36db3ac6bff2d23494356a7872bf84e8b5e
haider888-abbas <haidersherazi888@gmail.com>
2020-11-16 00:45:17 +0500

    HaiderChange

--- src/components/subheader/Search/Search.js
+++ src/components/subheader/Search/Search.js
@@ -3,9 +3,10 @@ import React from "react";
 const Search = () =>{
   return(
     <div>
-    <h2 style={{color:'red'}}>Hy I am Search Component</h2>
+    <h2 style={{color:'red'}}>Hy I am haider Search Component </h2>
     </div>
   );
 }
+
 
 export default Search;
