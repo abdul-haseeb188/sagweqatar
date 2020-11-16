46d9e86c5c54d7a569b3ddda00a98fe36b44435a
haider888-abbas <haidersherazi888@gmail.com>
2020-11-16 18:44:37 +0500

    changes

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,7 +3,10 @@
 const Search = () => {
   return (
     <div>
-      <input type="search" />
+      <input type="search" placeholder="Enter to search" />
+      <a href="#">
+        <i class= "fas fa-search" ></i>
+      </a>
       <h1>hi i am haider</h1>
 ssga      <p>I am sorry haider</p>
 <h2>this is search bar sir </h2>
