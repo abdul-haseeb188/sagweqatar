d90dafc4ed8dbac8590d7172264c4c7f116f81ee
Muhammad Qasim <41919676+qasim5611@users.noreply.github.com>
2020-11-16 19:20:39 +0500

    qasim

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,49 @@
 import React from 'react';
-const Header = () => {
+import DesiMarketLogo from '../../Images/kojE409403.jpg'; 
+import ShoppingCartSharpIcon from '@material-ui/icons/ShoppingCartSharp';
+import FavoriteSharpIcon from '@material-ui/icons/FavoriteSharp';
 
+
+import Grid from '@material-ui/core/Grid';
+
+
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const iconsstyl={
+    fontSize: '40px',
+    padding: '20px',
+}
+const Header = () => {
+   
     return (
 
         <div>
-            Tesing Phase
-            Hy welcome to  header From Qasim for testing to push on git
+         
+
+            <Grid container spacing={3}>
+               
+                <Grid item xs={3}>
+                    <img src={DesiMarketLogo} />
+                </Grid>
 
 
+                <Grid item xs={6}>
+                    Search Bar Will Show Here
+                </Grid>
+
+
+                <Grid item xs={3}>
+
+                    <Tooltip title="WishList" placement="bottom">
+                        <Button> <FavoriteSharpIcon style={iconsstyl} /></Button>
+                    </Tooltip>
+                    <Tooltip title="Add To Cart" placement="bottom">
+                        <Button>  <ShoppingCartSharpIcon style={iconsstyl} /></Button>
+                    </Tooltip>
+                   
+                </Grid>
+            </Grid>
         </div>
 
 
