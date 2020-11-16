67fc10523934046d6bf25d7f7d94863a98ac09e5
ravi <rochak.ravi@gmail.com>
2020-11-16 13:58:42 +0530

    mesg

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,11 @@
-import React from "react";
+ meimport React from "react";
 
 const Search = () => {
   return (
     <div>
       <input type="search" />
       <h1>hi i am haider</h1>
+ssga      <p>I am sorry haider</p>
     </div>
   );
 };
