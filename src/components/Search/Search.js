581cc4e48f8412f6336113537eac76e577971962
haider888-abbas <haidersherazi888@gmail.com>
2020-11-16 18:52:48 +0500

    changes

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,11 +3,11 @@
 const Search = () => {
   return (
     <div>
+
+      
       <input type="search" placeholder="Enter to search" />
-  
-      <h1>hi i am haider</h1>
-ssga      <p>I am sorry haider</p>
-<h2>this is search bar sir </h2>
+
+
     </div>
   );
 };
