63c48717ce9bd33b25a3565bf5e9456b509ea8e8
haider888-abbas <haidersherazi888@gmail.com>
2020-11-16 18:49:14 +0500

    changes

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,10 @@
- meimport React from "react";
+ import React from "react";
 
 const Search = () => {
   return (
     <div>
       <input type="search" placeholder="Enter to search" />
-      <a href="#">
-        <i class= "fas fa-search" ></i>
-      </a>
+  
       <h1>hi i am haider</h1>
 ssga      <p>I am sorry haider</p>
 <h2>this is search bar sir </h2>
